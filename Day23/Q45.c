103eff0f4ac60f7374eb917678b3d94bd5894240
Vedansh <vedanshrawat0307@gmail.com>
2025-09-12 13:16:01 +0530

    Update Q45.c

--- Day23/Q45.c
+++ Day23/Q45.c
@@ -1,6 +1,7 @@
+/*Q45 (Loops without Arrays/Strings)
+Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms. */
 
 #include <stdio.h>
-
 int main(){
     int n;
     float sum=0;
