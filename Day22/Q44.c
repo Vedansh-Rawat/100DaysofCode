fd659aef022b828cf685bfe9140a9796ecc99064
Vedansh <vedanshrawat0307@gmail.com>
2025-09-19 22:02:44 +0530

    commit Changes

--- Day22/Q44.c
+++ Day22/Q44.c
@@ -9,12 +9,12 @@ int main(){
     printf("Enter the value of n: \n");
     scanf("%d", &n);
 
-    for (int i = 1; i <= n; i++){
+    for (int i = 2; i <= n+1; i++){
         float numerator = 2*i - 1;
         float denominator = 2*i;
         float frac = numerator/denominator;
         sum = sum + frac;
     }
     sum++;
-    printf("The required sum is: %f", sum);
+    printf("The required sum is: %.2f", sum);
 }
