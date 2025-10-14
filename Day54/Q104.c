7b18eac46de565d9d6cf0199f523e0cf2f98dd9a
Vedansh <vedanshrawat0307@gmail.com>
2025-10-14 13:11:06 +0530

    commit Day54

--- Day54/Q104.c
+++ Day54/Q104.c
@@ -8,7 +8,11 @@ int main(){
   int n;
   printf("Enter a number: \n");
   scanf("%d", &n);
-
+  
+  if (n == 1){
+    printf("The pivot is 1\n");
+    return 0;
+  }
   // Getting the pivot
 
   int totSum = 0;
